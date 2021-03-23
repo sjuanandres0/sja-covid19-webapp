fdcfe7483ac93425c982ab53821a13d4987f4984
Juan <something@example.com>
2021-03-23 20:22:11 +0000

    fixing

--- myapp/routes.py
+++ myapp/routes.py
@@ -4,11 +4,6 @@ import json, plotly
 from flask import render_template, request, Response, jsonify
 from wrangling_scripts.wrangle_data import return_figures
 
-
-@app.route('/', methods=['POST', 'GET'])
-@app.route('/index', methods=['POST', 'GET'])
-def index():
-
 @app.route('/')
 @app.route('/index')
 def index():
